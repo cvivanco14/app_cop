0d46eb38a27a532b23949765877b6ad0e937620e
cvivanco14 <108816062+cvivanco14@users.noreply.github.com>
2022-09-06 18:37:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,7 @@ import string
 from openpyxl.styles import Border, Side, PatternFill
 import fun
 
-#dir_main = "C:\\Users\\Camila\\Documents\\Ploty\\"
-dir_main =  "/root/CMD/AppCami/"
-dir_data =  "/root/CMD/ECBS/Reportes/"
-dir_data2 =  "/root/CMD/ECBS/data/"
-
-
+dir_main = "C:/Users/camiv/Documents/App Marcus/"
 
 ###############################################################################
 #0. Inputs                                                                    #
@@ -57,88 +52,27 @@ input_02=["Responsable", "Región",'Comuna','Zona']  #Reporte 2
 old_cat = ["responsable"]
 new_cat = ["Responsable"]
 
-#Título pagina 
-titulo="Reportes Encuesta Complementaria de Bienestar Social"
+#Title
+titulo="Game's report draft"
 
-#Secciones reporte
+#Report sections
 sec01="**1. CDF de entrevista**"
 sec02="**2. Entrevistas completadas**"
 sec03=""
 
 
 # Usuarios
-d = {'user': ["admin","MDSF"], 'pass': ["Microdatos2020","MDSF2021"]}
+d = {'user': ["admin","inspector","fleet1","fleet2","fleet3"], 'pass': ["Admin2022","GOT2020","Arya2022","Sansa2021","Daenerys2020"]}
 df_users = pd.DataFrame(data=d)
 
-@st.cache(ttl=60*60, max_entries=20, suppress_st_warning=True,allow_output_mutation=True)
-def load_data(dir_data,variables):
-    data = pd.read_excel(os.path.join(dir_data+"Seguimiento2.xlsx"))
-    data = data.loc[ : ,variables]
-    data.loc[data['cdf'].isna(),'cdf']="520"
-
-      #Fecha
-    tz = pytz.timezone('Chile/Continental')
-    ahora = datetime.datetime.now(tz)
-
-    if ahora.month<10:
-     m = "0"+str(ahora.month)
-    if ahora.month>=10:
-     m = str(ahora.month)
-     date = str(ahora.year) + m + str(ahora.day)
-    if ahora.day<10:
-      date2 = str(ahora.year) + str("-")+m +  str("-0")+str(ahora.day)
-    if ahora.day>=10:
-      date2 = str(ahora.year) + str("-")+m +  str("-")+str(ahora.day)
-
-  #Formato fecha
-    data['Fecha'] = data['timestamp']
-    data.loc[data['Fecha'].isna(),'Fecha']=date2
-    data['Fecha'] = pd.to_datetime(data['Fecha'])
-    data['Fecha'] = data['Fecha'].dt.strftime('%d-%b-%Y')
-    data['cdf']=data['cdf'].astype(int)
-    data['cdf']=data['cdf'].astype(str)
-    data['cdf']=data['cdf']
-    del data['timestamp']
-
-    return data
-
-@st.cache(ttl=60*60, max_entries=20, suppress_st_warning=True,allow_output_mutation=True)
-def load_bitacora(dir_data):
-    data = pd.read_stata(os.path.join(dir_data+"Bitacora/Bitacora.dta"), convert_categoricals=False)
-    data=data.loc[data['folio']!='-1',:]
-    return data
-
-@st.cache(ttl=60*60, max_entries=20, suppress_st_warning=True,allow_output_mutation=True)
-def load_base(dir_data, dir_data2):
-    data = pd.read_stata(os.path.join(dir_data2+"Final_ecbs.dta"),convert_categoricals = False)
-    df = pd.read_excel(os.path.join(dir_data+"Seguimiento.xlsx"))
-    df=df.loc[:,["responsable","folio", 'dir_casen', 'telefono','edad_casen']]
-    data['folio'] = data['folio'].astype(int)
-    df['folio'] = df['folio'].astype(int)
-    data=pd.merge(data,df,on='folio', how='inner')
-    return data
-
-
-##Definir base Seguimiento 
-seguimiento = load_data(dir_data,variables)
-df=seguimiento
- 
-#Renombrar variables
-df.rename(columns=dict(zip(old_cat, new_cat)), inplace=True)
-
-#Bitacora
-bitacora = load_bitacora(dir_data)
-
-#Definir base final (sin cdf, solo 110)
-base = load_base(dir_data,dir_data2)
 
 
 
 ###############################################################################
-#1. Ingreso usuario                                                           #
+#1. User   login                                                              #
 ###############################################################################
 # Título
-image = Image.open(os.path.join(dir_main+"Imagen2.png"))
+image = Image.open(os.path.join(dir_main+"photo.jpg"))
 st.sidebar.image(image, channels="BGR")
     
 st.sidebar.title("Login")
@@ -181,3 +115,4 @@ if log == True:
   #Selección valor específico
   if pag_input=="Control y Seguimiento": 
    subseccion_input = st.sidebar.selectbox('Seleccionar reporte:', subseccion1)
+

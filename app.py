b2e5e51a47470c93d7383e29fea70b05e050e8a6
cvivanco14 <108816062+cvivanco14@users.noreply.github.com>
2022-09-17 15:54:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,38 +32,24 @@ dir_main = "/app/app_cop/"
 ###############################################################################
 #0. Inputs                                                                    #
 ###############################################################################
-
-
-x_input = ""
-y_input = ""
-f1_input = ""
-f2_input = ""
-
-#Variables relevantes
-#variables=['cdf', 'timestamp',"responsable_act","comuna_enc","region"] 
-variables=["folio",'cdf', 'timestamp',"responsable","Región", "Comuna",'Zona'] 
-
-#Variables selección base de datos 
-input_01=["Responsable", "Región",'Comuna','Zona']  #Reporte 1 
-input_02=["Responsable", "Región",'Comuna','Zona']  #Reporte 2
-
-
-#Input de categorias
-old_cat = ["responsable"]
-new_cat = ["Responsable"]
-
 #Title
-titulo="Game's report draft"
-
-#Report sections
-sec01="**1. CDF de entrevista**"
-sec02="**2. Entrevistas completadas**"
-sec03=""
+title="Game's report draft"
 
 
 # Usuarios
 d = {'user': ["admin","inspector","fleet1","fleet2","fleet3"], 'pass': ["Admin2022","GOT2020","Arya2022","Sansa2021","Daenerys2020"]}
 df_users = pd.DataFrame(data=d)
+
+
+#Dataframes
+#@st.cache(ttl=60*60, max_entries=20, suppress_st_warning=True,allow_output_mutation=True)
+#def load_score(dir_main):
+#    df = pd.read_csv(os.path.join(dir_main+"score.csv"), sep=";")
+#    return df
+    
+#df = load_score(dir_main)
+
+
 
 ###############################################################################
 #1. User   login                                                              #
@@ -97,16 +83,213 @@ if log == True:
               <hr style="border:1.75px solid black"> </hr>
               ''', unsafe_allow_html=True)
 
-  ###############################################################################
-  #2. Sección a seleccionar                                                     #
-  ###############################################################################
-  section= ["Day 1", "Day 2", 'Day 3','Day 4','Day 5','Day 6','Day 7','Day 8','Day 9','Day 10']
-  subsection1=["i. Type-in information", "ii. Overall Reports"]
 
+###############################################################################
+#A. Fleets                                                                    #
+###############################################################################
+
+###############################################################################
+#2. Section selection                                                         #
+###############################################################################
+  section= ["Day 1", "Day 2", 'Day 3','Day 4','Day 5','Day 6','Day 7','Day 8','Day 9','Day 10']
+  
   st.sidebar.title("Section selection")
   st.sidebar.markdown('''Select the day and section you wish to see.''')
-
+  
   #Selection of daily report 
   pag_input = st.sidebar.selectbox('Select day:', section)
-  #Selection of section in the daily report
-  subsection_input = st.sidebar.selectbox('Select report:', subsection1)
+
+
+
+###############################################################################
+#2.i Day 1                                                                    #
+###############################################################################
+  if pag_input=="Day 1": 
+    #Selection of section in the daily report
+   # Title
+   st.title(title)  
+   st.markdown('''
+              <hr style="border:1.75px solid black"> </hr>
+              ''', unsafe_allow_html=True)
+   st.header("**Day 1: Type in information**")
+   st.markdown('''In this subsection the fleet manager types in the score and the profit for every action (this is already
+accumulated)''')
+
+   with st.expander("Vessel scores"):
+#    if "df" not in st.session_state or score.columns[0]=="Unnamed: 0":
+    if "df" not in st.session_state:
+        st.session_state.df = pd.DataFrame(columns=["Vessel 1: Arya", 
+                                                    "Vessel 2: Cersei", 
+                                                    "Vessel 3: Lyanna", 
+                                                    "Vessel 4: Daenerys"])
+
+
+    st.markdown("Add Record")
+    
+    num_new_rows = 10
+    ncol = st.session_state.df.shape[1]  # col count
+    rw = -1
+    
+    with st.form(key="add form", clear_on_submit= True):
+        cols = st.columns(ncol)
+        rwdta = []
+    
+        for i in range(ncol):
+            rwdta.append(cols[i].text_input(st.session_state.df.columns[i]))
+    
+        # you can insert code for a list comprehension here to change the data (rwdta) 
+        # values into integer / float, if required
+    
+        if st.form_submit_button("Add"):
+            if st.session_state.df.shape[0] == num_new_rows:
+                st.error("Add row limit reached. Cant add any more records..")
+            else:
+                rw = st.session_state.df.shape[0] + 1
+                st.info(f"Row: {rw} / {num_new_rows} added")
+                st.session_state.df.loc[rw] = rwdta
+    
+                if st.session_state.df.shape[0] == num_new_rows:
+                    st.error("Add row limit reached...")
+    df = st.session_state.df
+    df.to_csv("score1.csv",encoding='utf-8',index=False)
+    st.text(df)
+    
+   with st.expander("Vessel profits"):
+    if "df2" not in st.session_state:
+        st.session_state.df2 = pd.DataFrame(columns=["Vessel 1: Arya", 
+                                                    "Vessel 2: Cersei", 
+                                                    "Vessel 3: Lyanna", 
+                                                    "Vessel 4: Daenerys"])
+    
+    st.markdown("Add Record")
+    
+    ncol = st.session_state.df2.shape[1]  # col count
+    rw = -1
+    
+    with st.form(key="add profits form", clear_on_submit= True):
+        cols = st.columns(ncol)
+        rwdta = []
+    
+        for i in range(ncol):
+            rwdta.append(cols[i].text_input(st.session_state.df2.columns[i]))
+    
+        # you can insert code for a list comprehension here to change the data (rwdta) 
+        # values into integer / float, if required
+    
+        if st.form_submit_button("Add"):
+            if st.session_state.df2.shape[0] == num_new_rows:
+                st.error("Add row limit reached. Cant add any more records..")
+            else:
+                rw = st.session_state.df2.shape[0] + 1
+                st.info(f"Row: {rw} / {num_new_rows} added")
+                st.session_state.df2.loc[rw] = rwdta
+    
+                if st.session_state.df2.shape[0] == num_new_rows:
+                    st.error("Add row limit reached...")
+    
+
+
+    df2 = st.session_state.df2
+    df2.to_csv("profits1.csv",encoding='utf-8',index=False)
+
+    st.text(st.session_state.df2)
+ 
+   with st.expander("Vessel fines"):
+    if "df3" not in st.session_state:
+        st.session_state.df3 = pd.DataFrame(columns=["Vessel 1: Arya", 
+                                                    "Vessel 2: Cersei", 
+                                                    "Vessel 3: Lyanna", 
+                                                    "Vessel 4: Daenerys"])
+    
+    st.markdown("Add Record")
+    
+    ncol = st.session_state.df3.shape[1]  # col count
+    rw = -1
+    
+    with st.form(key="add fines form", clear_on_submit= True):
+        cols = st.columns(ncol)
+        rwdta = []
+    
+        for i in range(ncol):
+            rwdta.append(cols[i].text_input(st.session_state.df3.columns[i]))
+    
+        # you can insert code for a list comprehension here to change the data (rwdta) 
+        # values into integer / float, if required
+    
+        if st.form_submit_button("Add"):
+            if st.session_state.df3.shape[0] == num_new_rows:
+                st.error("Add row limit reached. Cant add any more records..")
+            else:
+                rw = st.session_state.df3.shape[0] + 1
+                st.info(f"Row: {rw} / {num_new_rows} added")
+                st.session_state.df3.loc[rw] = rwdta
+    
+                if st.session_state.df3.shape[0] == num_new_rows:
+                    st.error("Add row limit reached...")
+    
+
+    df3 = st.session_state.df3
+    df3.to_csv("fines1.csv",encoding='utf-8',index=False)
+
+    st.text(st.session_state.df3)
+
+###############################################################################
+#2.i Overall reports                                                          #
+###############################################################################
+   st.markdown('''
+              <hr style="border:1.75px solid black"> </hr>
+              ''', unsafe_allow_html=True)
+   if st.button('Create overall reports'): 
+    st.header("**Day 1: Overall reports**")
+    st.markdown('''In this subsection the fleet manager can see the current accumulted score and profit of day 1 and the daily fleet scores and profits.''')
+    st.markdown('''
+              <hr style="border:1.75px solid black"> </hr>
+              ''', unsafe_allow_html=True)
+    #
+    df = pd.read_csv(os.path.join(dir_main,'score1.csv'), sep=',')
+    df2 = pd.read_csv(os.path.join(dir_main,'profits1.csv'), sep=',')
+    df3 = pd.read_csv(os.path.join(dir_main,'fines1.csv'), sep=',')
+    d = {'Vessel': df.columns[0:4] , 'Score':  df.iloc[-1] , 'Accumulated Profit':df2.iloc[-1] ,'Fines':df3.iloc[-1] }
+    #d = pd.DataFrame()
+    #d['Vessel']= df.columns[0:4]
+    #d['CDF'] = df.values[::-1]
+    #d['Score'] = df.iloc[-1] 
+    #d['Accumulated Profit'] = df2.iloc[-1] 
+    #['Fines'] = df3.iloc[-1] 
+    outcomes = pd.DataFrame(data=d)
+    outcomes=outcomes.reset_index(drop=True)
+    st.dataframe(outcomes)
+    #st.dataframe(d)
+    
+    #Plot scores 
+    df01 = pd.read_csv(os.path.join(dir_main,'score1.csv'), sep=',')
+    #Create the daily average 
+    df01['Fleet score']=df01.sum(axis=1)/4
+    df01['Action'] = df01.index
+    df01 = pd.melt(df01, id_vars='Action', value_vars=["Vessel 1: Arya", 
+                                                    "Vessel 2: Cersei", 
+                                                    "Vessel 3: Lyanna", 
+                                                    "Vessel 4: Daenerys", 
+                                                    "Fleet score"])
+    st.write(df01)
+    st.subheader("*Daily fleet and vessel scores*")
+    fig01 = px.line(df01, x='Action', y='value', color='variable', markers=True,range_x=[0,10],width=650,height=650)
+    fig01.update_layout(yaxis_type='category')
+    fig01.layout.xaxis.fixedrange= True
+    fig01.layout.yaxis.fixedrange: True
+    fig01.update(layout_showlegend=True)
+    st.plotly_chart(fig01)
+  
+    #Plot profits  
+    df02 = pd.read_csv(os.path.join(dir_main,'profits1.csv'), sep=',')
+    df02['Fleet profit']=df02.sum(axis=1)
+    df02['Action'] = df02.index
+    df02['Fleet score'] = df01.loc[df01.variable=="Fleet score",'value']
+    st.write(df02)
+#    df02['Profit w. score'] = 
+    df02 = pd.melt(df02, id_vars='Action', value_vars=["Fleet profit",
+                                                       "Fleet score"])
+    st.write(df02)
+    
+  #reset index from 1-10 
+  
